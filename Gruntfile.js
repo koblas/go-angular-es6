21a63e5e43b127508e10f2f30ebcb445beec1e21
David Koblas <david@tubularlabs.com>
2015-07-01 08:59:33 -0700

    Cleaned up tabbing

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,53 +1,54 @@
 module.exports = function (grunt) {
-   grunt.initConfig({
-      browserify: {
-         dist: {
-            options: {
-               transform: [
-                  ["babelify", {
-                     loose: "all",
-                     sourceMap: true,
-                  }], "partialify"
-               ],
-               extensions: ['.es6'],
-               debug: true
+    grunt.initConfig({
+        browserify: {
+            dist: {
+                options: {
+                    transform: [
+                        ["babelify", {
+                            loose: "all",
+                            sourceMap: true,
+                        }], "partialify",
+                    ],
+                    extensions: ['.es6'],
+                    debug: true,
+                    comments: true
+                },
+                files: {
+                    "./static/app.js": ["./client/app/main.es6"]
+                }
+            }
+        },
+        copy: {
+            all: {
+                // This copies all the html and css into the dist/ folder
+                expand: true,
+                cwd: 'client/',
+                src: ['**/*.css', 'components/**', 'bootstrap/**', 'app.html'],
+                dest: 'static/',
+              },
+        },
+        watch: {
+            grunt: {
+                files: ["Gruntfile.js"],
+                options: {
+                    reload: true
+                }
+            },
+            app: {
+                files: ["./client/app/**", "./client/partial/**"],
+                tasks: ["browserify"]
             },
             files: {
-               "./static/app.js": ["./client/app/main.es6"]
+                files: ['./client/**/*.css', './client/components/**', './client/bootstrap/**', './client/app.html'],
+                tasks: ["copy"]
             }
-         }
-      },
-      copy: {
-          all: {
-              // This copies all the html and css into the dist/ folder
-              expand: true,
-              cwd: 'client/',
-              src: ['**/*.css', 'components/**', 'bootstrap/**', 'app.html'],
-              dest: 'static/',
-          },
-      },
-      watch: {
-         grunt: {
-            files: ["Gruntfile.js"],
-            options: {
-                reload: true
-            }
-         },
-         app: {
-            files: ["./client/app/**", "./client/partial/**"],
-            tasks: ["browserify"]
-         },
-         files: {
-            files: ['./client/**/*.css', './client/components/**', './client/bootstrap/**', './client/app.html'],
-            tasks: ["copy"]
-         }
-      }
-   });
- 
-   grunt.loadNpmTasks("grunt-browserify");
-   grunt.loadNpmTasks("grunt-contrib-watch");
-   grunt.loadNpmTasks('grunt-contrib-copy');
- 
-   grunt.registerTask("default", ["watch"]);
-   grunt.registerTask("build", ["browserify", "copy"]);
+        }
+    });
+
+    grunt.loadNpmTasks("grunt-browserify");
+    grunt.loadNpmTasks("grunt-contrib-watch");
+    grunt.loadNpmTasks('grunt-contrib-copy');
+
+    grunt.registerTask("default", ["watch"]);
+    grunt.registerTask("build", ["browserify", "copy"]);
 };
